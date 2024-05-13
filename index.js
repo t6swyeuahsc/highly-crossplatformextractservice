a5bd53ad6cc60ce6c3acb0081ef30596eaa63f50
t6swyeuahsc <gs1jurlopes@hotmail.com>
2024-05-13 12:30:28 +0700

    test commit

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-function kthSmallest(matrix, k) {
-  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
-  flatten.sort((a, b) => a - b);
-  return flatten[k - 1];
-}
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
